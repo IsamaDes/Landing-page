3f2d98bcec55255e17af656ce89bbb31ab7b6fe8
Desmond Isama <desmond.isama@decagon.dev>
2024-06-15 19:08:11 +0100

    fixed button

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,73 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <div>
+    <form>
       <div className="contactPagebody">
-        <span className='contact'>Contact Us</span>
+        <span className="contact">Contact Us</span>
         <div className="firstnameAndLastname">
-          <div className='firstnameclass'>
-          <span className='first'>First Name</span>
-          <input type="firstname" placeholder="Enter your first name" />
+          <div className="firstnameclass">
+            <span className="first">First Name</span>
+            <input type="firstname" placeholder="Enter your first name" />
           </div>
-          <div className='lastnameclass'>
-          <span className='last'>Last Name</span>
-          <input type="lastname" placeholder="Enter your last name" />
+          <div className="lastnameclass">
+            <span className="last">Last Name</span>
+            <input type="lastname" placeholder="Enter your last name" />
           </div>
         </div>
         <div className="email">
-          <span className='emaila'>Email Address</span>
+          <label htmlfor="email">Email</label>
+
           <input
             type="email"
-            className="emailfield"
-            placeholder="Enter your email"
+            id="emailid"
+            pattern=".+@example\\\\.com"
+            size="{30}"
+            required
           />
         </div>
         <div className="querry">
-           <span className='querrytype'>Querry Type</span>
+          <span className="querrytype">Querry Type</span>
 
+          <div className="querryandsupport">
+            <div className="geradio">
+              <input
+                type="radio"
+                id="general-enquiry"
+                name="query-type"
+                className="query-radio"
+              />
+              <span>General Enquiry</span>
+            </div>
 
-          <div className='querryandsupport'>
-          
-          <div className='geradio'>
+            <div className="suppotio">
+              <input
+                type="radio"
+                id="support-request"
+                name="query-type"
+                className="query-radio"
+              />
 
-          <input type="radio" id="general-enquiry" name="query-type" className="query-radio" />
-          <span>General Enquiry</span>
+              <span>Support Request</span>
+            </div>
           </div>
+        </div>
 
-          <div className='suppotio'>
-           <input type="radio" id="support-request" name="query-type" className="query-radio" />
-        
-          <span>Support Request</span>
-          </div>
-          </div>
-        </div> 
-
-
-        <div className="message">
-          <span className='messa'>Message</span>
-          <input
-            type="messagefield"
-            className="messagefield"
-            
+        <div className="messageBox">
+          <label htmlFor="message">Message</label>
+          <br />
+          <textarea
+            id="message"
+            cols={80}
+            rows={8}
+            required
+            placeholder="My Name is Desmond Isama, am a fullstack software engineer."
           />
         </div>
-
-        
-        <div className='checkconsent'>
-        <input type="checkbox" id="myCheckbox" />
-        <span className='consent'>I consent to being contacted by the team</span>
-        </div>
-
-        <button className='submit' > Submit</button>
+        <input className="submitbutton" type="submit" value="Send Request" />
       </div>
-    </div>
+    </form>
   );
 }
 
-export default App
+export default App;
